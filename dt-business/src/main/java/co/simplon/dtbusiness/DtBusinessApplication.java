a0310b0b98ea94dd0ac4029403ccd7d1ae2464c1
Mohamed ABDALLAH <medaby@outlook.fr>
2024-08-26 15:22:36 +0200

    init business

---
+++ dt-business/src/main/java/co/simplon/dtbusiness/DtBusinessApplication.java
@@ -0,0 +1,13 @@
+package co.simplon.dtbusiness;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DtBusinessApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(DtBusinessApplication.class, args);
+    }
+
+}
