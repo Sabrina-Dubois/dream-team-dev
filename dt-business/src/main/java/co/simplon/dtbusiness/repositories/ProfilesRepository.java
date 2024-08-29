93437bdcda6795b2452e8b3ad50c224c6d0e24fd
Thao NGUYEN <phuongthaolyon@gmail.com>
2024-08-29 11:24:40 +0200

    create get profile by internal number

--- dt-business/src/main/java/co/simplon/dtbusiness/repositories/ProfilesRepository.java
+++ dt-business/src/main/java/co/simplon/dtbusiness/repositories/ProfilesRepository.java
@@ -1,12 +1,12 @@
 package co.simplon.dtbusiness.repositories;
 
 import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
 
 import co.simplon.dtbusiness.entities.ProfileEntity;
 
 @Repository
 public interface ProfilesRepository extends JpaRepository<ProfileEntity, Long> {
-    ProfileEntity findByInternalNumber(@Param(value = "internalNumber") String internalNumber);
+
+    ProfileEntity findByInternalNumber(String internalNumber);
 }
