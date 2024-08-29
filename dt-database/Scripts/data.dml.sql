d26e0571c1742e3fccb0a6408b8e12d037492e76
Thao NGUYEN <phuongthaolyon@gmail.com>
2024-08-29 11:15:14 +0200

    in progress

--- dt-database/Scripts/data.dml.sql
+++ dt-database/Scripts/data.dml.sql
@@ -8,7 +8,10 @@ INSERT INTO t_level_seniority (level_name) VALUES
 
 INSERT INTO t_profiles (internal_number, first_name, last_name, email, description, picture, linkedin)
 	VALUES
-	('ABC1234', 'Joe', 'Doe', 'joe@sg.com', 'super dev', 'http://image.jpn', 'linkedin.com' )
+	('abc1234', 'Joe', 'Doe', 'joe@sg.com', 'super dev', 'http://image.jpn', 'linkedin.com' ),
+	('a000001', 'Abdallah', 'Dev', 'abdallah@sg.com', NULL, NULL, NULL),
+	('a000002', 'Sabrina', 'Dubois', 'abdallah@sg.com', NULL, NULL, NULL),
+	('a000003', 'Thao', 'NGUYEN', 'abdallah@sg.com', NULL, NULL, NULL)
 	;
 
 INSERT INTO t_skills (skill_name, is_technical_skill ) VALUES 
@@ -18,3 +21,4 @@ INSERT INTO t_skills (skill_name, is_technical_skill ) VALUES
 INSERT INTO t_profile_skill_level(profile_id, skill_id, level_id ) VALUES 
 	(1,1,2);
 
+SELECT * FROM t_profiles tp ;
