56cda569e395110fe183f2c1cb293160da29cd01
Mohamed ABDALLAH <medaby@outlook.fr>
2024-08-30 01:00:10 +0200

    fix: rename field linkeding developers

---
+++ dt-business/src/main/java/co/simplon/dtbusiness/dtos/in/DeveloperUpdate.java
@@ -0,0 +1,6 @@
+package co.simplon.dtbusiness.dtos.out;
+
+public record DeveloperView(String internalNumber, String firstName, String lastName, String email, String Description,
+                            String picture, String linkedin) {
+
+}
---
+++ dt-business/src/main/java/co/simplon/dtbusiness/dtos/in/SkillCreateOrAdd.java
@@ -0,0 +1,4 @@
+package co.simplon.dtbusiness.dtos.in;
+
+public record TopicCreateOrAdd(Long id, String name,boolean isTechnical) {
+}
