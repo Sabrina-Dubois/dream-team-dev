805b951a7dc184824f6ec600e43f0e196d3c7f26
Thao NGUYEN <phuongthaolyon@gmail.com>
2024-08-30 11:39:06 +0200

    fix validation in back

--- dt-business/src/main/java/co/simplon/dtbusiness/controllers/DeveloperController.java
+++ dt-business/src/main/java/co/simplon/dtbusiness/controllers/DeveloperController.java
@@ -2,11 +2,18 @@ package co.simplon.dtbusiness.controllers;
 
 import java.util.List;
 
-import co.simplon.dtbusiness.dtos.in.DeveloperUpdate;
-import org.springframework.web.bind.annotation.*;
+import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PatchMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
 
+import co.simplon.dtbusiness.dtos.in.DeveloperUpdate;
 import co.simplon.dtbusiness.dtos.out.DeveloperView;
 import co.simplon.dtbusiness.services.DeveloperService;
+import jakarta.validation.Valid;
 
 @CrossOrigin("*")
 @RequestMapping("/developers")
@@ -29,9 +36,9 @@ public class DeveloperController {
     }
 
     @PatchMapping("/{internalNumber}")
-    public void updateDeveloper(@PathVariable("internalNumber") String internalNumber, @ModelAttribute  final DeveloperUpdate developer) {
-        service.updateDeveloper(internalNumber,developer);
+    public void updateDeveloper(@PathVariable("internalNumber") String internalNumber,
+	    @Valid @ModelAttribute final DeveloperUpdate developer) {
+	service.updateDeveloper(internalNumber, developer);
     }
-
 
 }
--- dt-business/src/main/java/co/simplon/dtbusiness/dtos/in/DeveloperUpdate.java
+++ dt-business/src/main/java/co/simplon/dtbusiness/dtos/in/DeveloperUpdate.java
@@ -1,14 +1,19 @@
 package co.simplon.dtbusiness.dtos.in;
 
-import org.springframework.web.multipart.MultipartFile;
-
 import java.util.Set;
 
-public record DeveloperUpdate(
-        String description,
-        MultipartFile picture,
-        String linkedin,
-        Set<SkillCreateOrAdd> skills
-        ) {
+import org.springframework.http.MediaType;
+import org.springframework.web.multipart.MultipartFile;
+
+import co.simplon.dtbusiness.dtos.validators.FileSize;
+import co.simplon.dtbusiness.dtos.validators.FileType;
+import jakarta.validation.constraints.Pattern;
+import jakarta.validation.constraints.Size;
+
+public record DeveloperUpdate(@Size(max = 5000) String description, @FileType(types = {
+	MediaType.IMAGE_GIF_VALUE, MediaType.IMAGE_JPEG_VALUE,
+	MediaType.IMAGE_PNG_VALUE }) @FileSize(max = FileSize.ONE_MB) MultipartFile picture,
+	@Size(max = 200) @Pattern(regexp = "^(https?://)?(www\\.)?linkedin\\.com/.*$", message = "Invalid LinkedIn URL") String linkedin,
+	Set<SkillCreateOrAdd> skills){
 
 }
