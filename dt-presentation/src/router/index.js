a65a1442e9a22b34f62f4041f39e69b5f5aae1a4
Thao NGUYEN <phuongthaolyon@gmail.com>
2024-10-22 15:54:58 +0200

    in progress

--- dt-presentation/src/router/index.js
+++ dt-presentation/src/router/index.js
@@ -1,27 +1,28 @@
-import {createRouter, createWebHistory} from "vue-router";
+import { createRouter, createWebHistory } from "vue-router";
+
 import Profile from "../views/Profile.vue";
 import SearchDevelopers from "@/views/SearchDevelopers.vue";
 import Filters from "../views/Filters.vue";
 
 const router = createRouter({
-    history: createWebHistory(import.meta.env.BASE_URL),
-    routes: [
-        {
-            path: "/",
-            name: "Profile",
-            component: Profile,
-        },
-        {
-            path: "/search",
-            name: "Search developers",
-            component: SearchDevelopers,
-        },
-        {
-            path: "/filters",
-            name: "Filters",
-            component: Filters,
-        },
-    ],
+	history: createWebHistory(import.meta.env.BASE_URL),
+	routes: [
+		{
+			path: "/",
+			name: "Profile",
+			component: Profile,
+		},
+		{
+			path: "/search",
+			name: "Search developers",
+			component: SearchDevelopers,
+		},
+		{
+			path: "/filters",
+			name: "Filters",
+			component: Filters,
+		},
+	],
 });
 
 export default router;
